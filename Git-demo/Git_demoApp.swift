e0e350d0e796e027dae0ecdf300ee872e25bf5bf
Lukáš Skřivánek <luky.skrivos@gmail.com>
2024-02-22 10:02:23 +0100

    Initial Commit

---
+++ Git-demo/Git_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_demoApp.swift
+//  Git-demo
+//
+//  Created by macbook on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Git_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
